16e6790771b4393bca756b7e9a36c9c328201339
Esmanur Ak <esmanurhavinak@gmail.com>
2026-07-15 17:28:31 +0300

    Port degeri dinamik hale getirildi

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const { sertifikaPdfUret } = require('./sertifikaOlustur');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Görsellerin kaydedileceği klasörü oluşturuyoruz (yoksa)
 const uploadDir = path.join(__dirname, 'uploads', 'referanslar');
